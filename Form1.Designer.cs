1e146fcf789c1a82df89c1807d996e053153ff10
Behfar Lotfizadeh <behfar@otafest.com>
2021-03-21 01:00:06 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace FF8_Save_Swapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list_saves = new System.Windows.Forms.ListBox();
+            this.btn_swap = new System.Windows.Forms.Button();
+            this.lbl_explanation = new System.Windows.Forms.Label();
+            this.btn_setSaveFolder = new System.Windows.Forms.Button();
+            this.lbl_saveFolder = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // list_saves
+            // 
+            this.list_saves.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list_saves.FormattingEnabled = true;
+            this.list_saves.Location = new System.Drawing.Point(12, 12);
+            this.list_saves.Name = "list_saves";
+            this.list_saves.Size = new System.Drawing.Size(491, 459);
+            this.list_saves.TabIndex = 0;
+            // 
+            // btn_swap
+            // 
+            this.btn_swap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_swap.Location = new System.Drawing.Point(12, 506);
+            this.btn_swap.Name = "btn_swap";
+            this.btn_swap.Size = new System.Drawing.Size(92, 23);
+            this.btn_swap.TabIndex = 1;
+            this.btn_swap.Text = "Go";
+            this.btn_swap.UseVisualStyleBackColor = true;
+            this.btn_swap.Click += new System.EventHandler(this.btn_swap_Click);
+            // 
+            // lbl_explanation
+            // 
+            this.lbl_explanation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbl_explanation.AutoSize = true;
+            this.lbl_explanation.Location = new System.Drawing.Point(110, 511);
+            this.lbl_explanation.Name = "lbl_explanation";
+            this.lbl_explanation.Size = new System.Drawing.Size(238, 13);
+            this.lbl_explanation.TabIndex = 2;
+            this.lbl_explanation.Text = "Clicking Go will replace the save in card 2 slot 28";
+            // 
+            // btn_setSaveFolder
+            // 
+            this.btn_setSaveFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_setSaveFolder.Location = new System.Drawing.Point(12, 477);
+            this.btn_setSaveFolder.Name = "btn_setSaveFolder";
+            this.btn_setSaveFolder.Size = new System.Drawing.Size(92, 23);
+            this.btn_setSaveFolder.TabIndex = 5;
+            this.btn_setSaveFolder.Text = "Set User Folder";
+            this.btn_setSaveFolder.UseVisualStyleBackColor = true;
+            this.btn_setSaveFolder.Click += new System.EventHandler(this.btn_setSaveFolder_Click);
+            // 
+            // lbl_saveFolder
+            // 
+            this.lbl_saveFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_saveFolder.AutoSize = true;
+            this.lbl_saveFolder.Location = new System.Drawing.Point(110, 482);
+            this.lbl_saveFolder.Name = "lbl_saveFolder";
+            this.lbl_saveFolder.Size = new System.Drawing.Size(211, 13);
+            this.lbl_saveFolder.TabIndex = 6;
+            this.lbl_saveFolder.Text = "<-- Click first to select your FF8 saves folder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 541);
+            this.Controls.Add(this.lbl_saveFolder);
+            this.Controls.Add(this.btn_setSaveFolder);
+            this.Controls.Add(this.lbl_explanation);
+            this.Controls.Add(this.btn_swap);
+            this.Controls.Add(this.list_saves);
+            this.MinimumSize = new System.Drawing.Size(531, 580);
+            this.Name = "Form1";
+            this.Text = "FFVIII Save Swapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list_saves;
+        private System.Windows.Forms.Button btn_swap;
+        private System.Windows.Forms.Label lbl_explanation;
+        private System.Windows.Forms.Button btn_setSaveFolder;
+        private System.Windows.Forms.Label lbl_saveFolder;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FF8_Save_Swapper
+{
+    public partial class Form1 : Form
+    {
+        public static string saveFolder;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListSaves();
+        }
+
+        private void ListSaves()
+        {
+            Dictionary<string, string> saves = new Dictionary<string, string>();
+
+            string[] allfiles = Directory.GetFiles(".\\saves", "*.ff8", SearchOption.AllDirectories);
+            foreach (var file in allfiles)
+            {
+                FileInfo info = new FileInfo(file);
+                // Do something with the Folder or just add them to a list via nameoflist.add();
+
+                var filename = info.Name;
+                var directory = info.Directory.Name;
+                var parent = info.Directory.Parent.Name;
+
+                var listItem = (parent != "saves") ? parent + " --- " + directory : directory;
+                var listItemWithFileName = listItem + "--" + filename.Substring(0, filename.Length - 4);
+
+                saves.Add(info.FullName, listItemWithFileName);
+            }
+
+            // Shutdown the painting of the ListBox as items are added.
+            list_saves.BeginUpdate();
+
+            list_saves.DataSource = new BindingSource(saves, null);
+            list_saves.DisplayMember = "Value";
+            list_saves.ValueMember = "Key";
+
+            // Allow the ListBox to repaint and display the new items.
+            list_saves.EndUpdate();
+        }
+
+        private void ReplaceSaveFile(string sourceFile)
+        {
+            // If no save folder, set one first.
+            if (String.IsNullOrEmpty(saveFolder))
+            {
+                MessageBox.Show("Set your FF8 save folder first.");
+                SetSaveFolder();
+            }
+
+            string FF8SaveFolder = saveFolder + "\\slot2_save28.ff8";
+            File.Copy(sourceFile, FF8SaveFolder, true);
+        }
+
+        private void btn_swap_Click(object sender, EventArgs e)
+        {
+            KeyValuePair<string, string> selectedItem = (KeyValuePair<string, string>)list_saves.SelectedItem;
+            var file = selectedItem.Key;
+            ReplaceSaveFile(file);
+        }
+
+        private void btn_setSaveFolder_Click(object sender, EventArgs e)
+        {
+            SetSaveFolder();
+        }
+
+        private void SetSaveFolder()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Square Enix\\FINAL FANTASY VIII Steam\\";
+            dialog.IsFolderPicker = true;
+            dialog.Title = "Select your user_xxxxxxx folder where your FF8 saves are located.";
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                saveFolder = dialog.FileName;
+                lbl_saveFolder.Text = dialog.FileName;
+            }
+        }
+    }
+}
